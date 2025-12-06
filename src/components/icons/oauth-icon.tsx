8fdbde1e39ab2e13a102c216a274a009a1199a43
Arsfy <arsfy@outlook.com>
2025-12-06 16:36:15 +0800

    feat: add support for additional OAuth providers including GitLab, Discord, Microsoft, Meta Facebook, X (Twitter), and LinkedIn

--- src/components/icons/oauth-icon.tsx
+++ src/components/icons/oauth-icon.tsx
@@ -3,12 +3,16 @@ import { KeyRound } from 'lucide-react';
 const OAuthIcon = ({ icon }: { icon: string }) =>
     icon.startsWith('http') ? (
         <img src={icon} className="w-4.5" alt={icon} />
-    ) : ['google', 'github'].includes(icon.toLowerCase()) ? (
+    ) : ['google', 'github', 'discord'].includes(icon.toLowerCase()) ? (
         <img
             src={'/icons/' + icon.toLowerCase() + '.svg'}
             className="w-4.5 bg-white rounded-full"
             alt={icon}
         />
+    ) : ['gitlab', 'discord', 'microsoft', 'meta', 'x', 'linkedin'].includes(icon.toLowerCase()) ? (
+        <div className={'w-4.5 p-0.5 bg-white rounded-full overflow-hidden'}>
+            <img src={'/icons/' + icon.toLowerCase() + '.svg'} className=" bg-white" alt={icon} />
+        </div>
     ) : (
         <KeyRound />
     );
